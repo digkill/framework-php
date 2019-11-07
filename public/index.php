4e69faac00f82a2170737bd9bc3a69daa2269f03
Edifanoff <vitaliy@edifanov.com>
2019-11-07 17:57:34 +0300

    Added class Response

--- public/index.php
+++ public/index.php
@@ -1,16 +1,23 @@
 <?php
 
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 use Framework\Http\RequestFactory;
+use Framework\Http\Response;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
-
-$request = (new Request())
-    ->withQueryParams($_GET)
-    ->withParsedBody($_POST);
-
 $request = RequestFactory::fromGlobals();
 
 $name = $request->getQueryParams()['name'] ?? 'Guest';
-header('X-Developer: Edifanoff');
-echo 'Hello, ' . $name . '!';
+
+$response = (new Response('Hello, ' . $name . '!'))
+    ->withHeader('X-Developer', 'Edifanoff');
+
+header('HTTP/1.0 ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+foreach ($response->getHeaders() as $name => $value) {
+    header($name . ':' . $value);
+}
+echo $response->getBody();
